aedd14fe084904e6cbac43741b1fdc736fff6913
Mohamed Essam <68747362+mohamedES24@users.noreply.github.com>
2024-08-13 17:17:18 +0300

    edit table style

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import WarningCard from './WarningCard';
 import { validateProduct } from '../utils/Validation';
+
 const ProductTable = ({ products, updateProduct, deleteProduct }) => {
     const [editingProduct, setEditingProduct] = useState(null);
     const [editedProduct, setEditedProduct] = useState({});
@@ -18,7 +19,6 @@ const ProductTable = ({ products, updateProduct, deleteProduct }) => {
     const handleChange = (e) => {
         const { name, value } = e.target;
 
-        // Prevent leading zero in price
         if (name === 'price') {
             if (value === '' || /^[0-9]*\.?[0-9]*$/.test(value)) {
                 setEditedProduct((prev) => ({ ...prev, [name]: value }));
@@ -30,11 +30,10 @@ const ProductTable = ({ products, updateProduct, deleteProduct }) => {
 
     const handleUpdate = async (productId) => {
         const validationErrors = validateProduct({ editedProduct }, 'editedProduct');
-        
         if (Object.keys(validationErrors).length === 0) {
             const updatedProduct = {
                 ...editedProduct,
-                updatedAt: new Date().toISOString() // Set updatedAt to current date
+                updatedAt: new Date().toISOString()
             };
             await updateProduct(productId, updatedProduct);
             setEditingProduct(null);
@@ -42,16 +41,10 @@ const ProductTable = ({ products, updateProduct, deleteProduct }) => {
         } else {
             setErrors((prevErrors) => ({
                 ...prevErrors,
-                [productId]: validationErrors // Ensure this line correctly maps errors to the productId
+                [productId]: validationErrors
             }));
         }
     };
-    
-    
-    const handleCancelEdit = () =>{
-        setEditingProduct(null);
-        setErrors({});
-    }
 
     const handleDeleteClick = (productId) => {
         setProductToDelete(productId);
@@ -70,7 +63,7 @@ const ProductTable = ({ products, updateProduct, deleteProduct }) => {
     };
 
     return (
-        <div>
+        <div className="overflow-x-auto">
             {showWarningCard && (
                 <WarningCard
                     message="Are you sure you want to delete this product?"
@@ -78,8 +71,7 @@ const ProductTable = ({ products, updateProduct, deleteProduct }) => {
                     onCancel={cancelDelete}
                 />
             )}
-
-            <table className="min-w-full bg-white border border-gray-300">
+            <table className="min-w-full bg-white border border-gray-300 text-sm md:text-base">
                 <thead>
                     <tr>
                         <th className="py-2 px-4 border-b">Name</th>
@@ -93,49 +85,61 @@ const ProductTable = ({ products, updateProduct, deleteProduct }) => {
                     </tr>
                 </thead>
                 <tbody>
-                    {products.map(product => (
+                    {products.map((product) => (
                         <tr key={product.id}>
                             <td className="py-2 px-4 border-b">
                                 {editingProduct === product.id ? (
-                                    <input
-                                        type="text"
-                                        name="name"
-                                        value={editedProduct.name}
-                                        onChange={handleChange}
-                                        className={`p-2 border border-gray-300 rounded w-full ${errors.name ? 'border-red-500' : ''}`}
-                                    />
+                                    <>
+                                        <input
+                                            type="text"
+                                            name="name"
+                                            value={editedProduct.name}
+                                            onChange={handleChange}
+                                            className={`p-2 border border-gray-300 rounded w-full ${errors[product.id]?.name ? 'border-red-500' : ''}`}
+                                        />
+                                        {errors[product.id]?.name && (
+                                            <p className="text-red-500 text-xs">{errors[product.id].name}</p>
+                                        )}
+                                    </>
                                 ) : (
                                     product.name
                                 )}
-                                {errors[product.id]?.name && <p className="text-red-500 text-sm">{errors[product.id].name}</p>}
                             </td>
                             <td className="py-2 px-4 border-b">
                                 {editingProduct === product.id ? (
-                                    <input
-                                        type="text"
-                                        name="description"
-                                        value={editedProduct.description}
-                                        onChange={handleChange}
-                                        className={`p-2 border border-gray-300 rounded w-full ${errors.description ? 'border-red-500' : ''}`}
-                                    />
+                                    <>
+                                        <input
+                                            type="text"
+                                            name="description"
+                                            value={editedProduct.description}
+                                            onChange={handleChange}
+                                            className={`p-2 border border-gray-300 rounded w-full ${errors[product.id]?.description ? 'border-red-500' : ''}`}
+                                        />
+                                        {errors[product.id]?.description && (
+                                            <p className="text-red-500 text-xs">{errors[product.id].description}</p>
+                                        )}
+                                    </>
                                 ) : (
                                     product.description
                                 )}
-                                {errors[product.id]?.description && <p className="text-red-500 text-sm">{errors[product.id].description}</p>}
                             </td>
                             <td className="py-2 px-4 border-b">
                                 {editingProduct === product.id ? (
-                                    <input
-                                        type="text"
-                                        name="price"
-                                        value={editedProduct.price}
-                                        onChange={handleChange}
-                                        className={`p-2 border border-gray-300 rounded w-full ${errors.price ? 'border-red-500' : ''}`}
-                                    />
+                                    <>
+                                        <input
+                                            type="text"
+                                            name="price"
+                                            value={editedProduct.price}
+                                            onChange={handleChange}
+                                            className={`p-2 border border-gray-300 rounded w-full ${errors[product.id]?.price ? 'border-red-500' : ''}`}
+                                        />
+                                        {errors[product.id]?.price && (
+                                            <p className="text-red-500 text-xs">{errors[product.id].price}</p>
+                                        )}
+                                    </>
                                 ) : (
                                     `$${Number(product.price).toFixed(2)}`
                                 )}
-                                {errors[product.id]?.price && <p className="text-red-500 text-sm">{errors[product.id].price}</p>}
                             </td>
                             <td className="py-2 px-4 border-b">
                                 {editingProduct === product.id ? (
@@ -144,36 +148,36 @@ const ProductTable = ({ products, updateProduct, deleteProduct }) => {
                                         name="releaseDate"
                                         value={editedProduct.releaseDate}
                                         onChange={handleChange}
-                                        className={`p-2 border border-gray-300 rounded w-full ${errors.releaseDate ? 'border-red-500' : ''}`}
+                                        className={`p-2 border border-gray-300 rounded w-full ${errors[product.id]?.releaseDate ? 'border-red-500' : ''}`}
                                     />
                                 ) : (
                                     product.releaseDate
                                 )}
-                                {errors[product.id]?.releaseDate && <p className="text-red-500 text-sm">{errors[product.id].releaseDate}</p>}
+                                {errors[product.id]?.releaseDate && <p className="text-red-500 text-xs">{errors[product.id].releaseDate}</p>}
                             </td>
                             <td className="py-2 px-4 border-b">
                                 {editingProduct === product.id ? (
-                                    <input
-                                        type="text"
-                                        name="category"
-                                        value={editedProduct.category}
-                                        onChange={handleChange}
-                                        className={`p-2 border border-gray-300 rounded w-full ${errors.category ? 'border-red-500' : ''}`}
-                                    />
+                                    <>
+                                        <input
+                                            type="text"
+                                            name="category"
+                                            value={editedProduct.category}
+                                            onChange={handleChange}
+                                            className={`p-2 border border-gray-300 rounded w-full ${errors[product.id]?.category ? 'border-red-500' : ''}`}
+                                        />
+                                        {errors[product.id]?.category && (
+                                            <p className="text-red-500 text-xs">{errors[product.id].category}</p>
+                                        )}
+                                    </>
                                 ) : (
                                     product.category
                                 )}
-                                {errors[product.id]?.category && <p className="text-red-500 text-sm">{errors[product.id].category}</p>}
                             </td>
                             <td className="py-2 px-4 border-b">
                                 {product.createdAt}
                             </td>
                             <td className="py-2 px-4 border-b">
-                                {editingProduct === product.id ? (
-                                    'Not Edited Yet'
-                                ) : (
-                                    product.updatedAt
-                                )}
+                                {product.updatedAt || 'Not Edited Yet'}
                             </td>
                             <td className="py-2 px-4 border-b">
                                 {editingProduct === product.id ? (
@@ -186,13 +190,13 @@ const ProductTable = ({ products, updateProduct, deleteProduct }) => {
                                         </button>
                                         <button
                                             className="bg-gray-500 text-white px-4 py-2 rounded"
-                                            onClick={handleCancelEdit}
+                                            onClick={() => setEditingProduct(null)}
                                         >
                                             Cancel
                                         </button>
                                     </div>
                                 ) : (
-                                    <div className="flex gap-2">
+                                    <div className="flex flex-col gap-2 sm:flex-row">
                                         <Link
                                             to={`/products/${product.id}`}
                                             className="bg-blue-500 text-white px-4 py-2 rounded inline-block text-center"
